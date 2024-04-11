df815afc71c07e3a59457431ab4bcf50a68be19d
bijesh <bijesh.shrestha531@gmail.com>
2024-04-11 14:50:32 -0400

    Removing Unnecessary files

--- application.py
+++
@@ -1,7 +0,0 @@
-def prediction(image_input):
-    
-    # Placeholder return for now. Replace with aayush's code
-    species = "tomato"
-    disease = "Leaf fungus"
-    
-    return species, disease
